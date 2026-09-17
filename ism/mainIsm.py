645c54f0c2d9cf4e84f68108a1adffac86d8f18a
Marco <100429690@alumnos.uc3m.es>
2026-09-17 20:25:34 +0200

    cambio final de clase 2

--- ism/mainIsm.py
+++ ism/mainIsm.py
@@ -5,8 +5,8 @@ from ism.src.ism import ism
 
 # Directory - this is the common directory for the execution of the E2E, all modules
 auxdir = r'C:\\Users\\marco\\OneDrive\\Escritorio\\PROCESADO DE DATOS TIERRA\\EODP_codigo\\auxiliary'
-indir = r"C:\\Users\\marco\\OneDrive\\Escritorio\\PROCESADO DE DATOS TIERRA\\EODP_TER_2021-20260910T154758Z-1-001\\EODP_TER_2021\\EODP-TS-L1B\\input" # small scene
-outdir = r"C:\\Users\\marco\\OneDrive\\Escritorio\\PROCESADO DE DATOS TIERRA\\EODP_TER_2021-20260910T154758Z-1-001\\EODP_TER_2021\\EODP-TS-L1B\\output_test_marco"
+indir = r"C:\\Users\\marco\\OneDrive\\Escritorio\\PROCESADO DE DATOS TIERRA\\EODP_TER_2021-20260910T154758Z-1-001\\EODP_TER_2021\\EODP-TS-ISM\\input\\gradient_alt100_act150" # small scene
+outdir = r"C:\\Users\\marco\\OneDrive\\Escritorio\\PROCESADO DE DATOS TIERRA\\EODP_TER_2021-20260910T154758Z-1-001\\EODP_TER_2021\\EODP-TS-ISM\\output_test_marco"
 
 # Initialise the ISM
 myIsm = ism(auxdir, indir, outdir)
